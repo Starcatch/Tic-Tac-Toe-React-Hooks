e7e7237a0dec7138eddc4e4f7cc6d2610bdf7f6b
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-02 18:33:33 -0500

    add an  alert to squares

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./TicTacToe.css";
 
 const TicTacToe = () => {
-	const Cell = () => {
-		return <td> - </td>;
+	const handleClick = (num) => {
+		alert("test");
+	};
+	const Cell = ({ num }) => {
+		return <td onClick={() => handleClick(num)}> - </td>;
 	};
 
 	return (
@@ -11,19 +14,19 @@ const TicTacToe = () => {
 			<table>
 				<tbody>
 					<tr>
-						<Cell />
-						<Cell />
-						<Cell />
+						<Cell num={0} />
+						<Cell num={1} />
+						<Cell num={2} />
 					</tr>
 					<tr>
-						<Cell />
-						<Cell />
-						<Cell />
+						<Cell num={3} />
+						<Cell num={4} />
+						<Cell num={6} />
 					</tr>
 					<tr>
-						<Cell />
-						<Cell />
-						<Cell />
+						<Cell num={7} />
+						<Cell num={8} />
+						<Cell num={9} />
 					</tr>
 				</tbody>
 			</table>
