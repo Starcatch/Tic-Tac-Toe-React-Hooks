97ef3c7b22458c97ac933e580040684aac0ea57c
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-02 19:00:43 -0500

    add winning combinations

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -3,18 +3,50 @@ import "./TicTacToe.css";
 
 const TicTacToe = () => {
 	const [turn, setTurn] = useState("x");
+	const [cells, setCells] = useState(Array(9).fill(""));
+
+	const checkForWinner = () => {
+		let combos = {
+			across: [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+			],
+			down: [
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+			],
+			diagonal: [
+				[0, 4, 8],
+				[2, 4, 6],
+			],
+		};
+	};
 
 	const handleClick = (num) => {
+		if (cells[num] !== "") {
+			alert("already clicked");
+			return;
+		}
+
+		let squares = [...cells];
 
 		if (turn === "x") {
+			squares[num] = "x";
 			setTurn("o");
 		} else {
+			squares[num] = "o";
 			setTurn("x");
 		}
+
+		checkForWinner(squares);
+		setCells(squares);
+		// console.log(squares);
 	};
 
 	const Cell = ({ num }) => {
-		return <td onClick={() => handleClick(num)}> - </td>;
+		return <td onClick={() => handleClick(num)}>{cells[num]} </td>;
 	};
 
 	return (
